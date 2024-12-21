e42706cfb23243a2c193391c9311bc81d8e2ab2a
A8E1 <98847922+A8E1@users.noreply.github.com>
2024-12-21 12:35:35 -0600

    added port for app to listen in on

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 
+//uses express package in file
 const express = require('express')
+
+
+//express app
+const app = express()
+
+//ROUTES
+app.get('/', (req, res)=>{
+    res.json({mssg: "mama we made it"})
+})
+
+//listen 4 requests @ port #
+app.listen(8000, () => {
+    console.log('listening on port 8000')
+})
